0bdc2d3f53b4df118f0bf4865a9cb0dc123a48da
Krugman Artem <artem.krugman14@gmail.com>
2022-11-12 12:55:26 +0300

    add task resolve

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary > 0) {
+            double taxes;
+            if (salary <= 10000) {
+                taxes = 0.15;
+            } else {
+                if (salary <= 20000) {
+                    taxes = 0.18;
+                } else {
+                    taxes = 0.2;
+                }
+            }
+            System.out.println(Math.round(salary * (1 - taxes) * 10 )/ 10.0);
+        } else {
+            System.out.println("wrong input!");
+        }
     }
 }
